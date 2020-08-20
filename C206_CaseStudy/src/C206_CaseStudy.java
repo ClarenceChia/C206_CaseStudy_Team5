52813e96edd51283c121c2f732bc0ede0a74f4ec
roccoyu <roccoyu@macintosh>
2020-08-20 14:26:41 +0800

    member5 checked

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -11,65 +11,66 @@ import java.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
 
-
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class C206_CaseStudy {
-	
+
 	private static final int OPTION_QUIT = 5;
-	static ArrayList <registerSchedule> list = new ArrayList<registerSchedule>();
+	static ArrayList<registerSchedule> reglist = new ArrayList<registerSchedule>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		ArrayList<Course> courses = new ArrayList<>();
 		ArrayList<Course> CategoryList = new ArrayList<Course>();
-		
+
 		int option = 0;
 
 		while (option != OPTION_QUIT) {
 
-			//menu
+			// menu
 			menu();
-			option =  Helper.readInt("Enter an option > ");
+			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
-				//view courses
+				// view courses
 				viewCourses(courses);
-				
+
 			} else if (option == 2) {
 
 				//
 				// add course
 				addCourse(courses);
 
-
 			} else if (option == 3) {
 				// delete course
 				deleteCourse(courses);
 
 			} else if (option == 4) {
-				//d
+				// Registration Schedule
 				regSche();
 				viewAllr();
+				char x = Helper.readChar("\nDo you want to delete any Registration ? (y/n) > ");
+				if (x == 'y' || x == 'Y') {
+					delr();
+					viewAllr();
 
-			} 
-			
-			else if (option == OPTION_QUIT) {
-				System.out.println("Bye!");
-				
-			} else {
-				System.out.println("Invalid option");
+				}
+
+				else if (option == OPTION_QUIT) {
+					System.out.println("Bye!");
+
+				} else {
+					System.out.println("Invalid option");
+				}
+
 			}
-
 		}
 
-	} //main
-	
-	
+	} // main
+
 	private static void menu() {
 		setHeader("COURSE APP");
 		System.out.println("1. View Courses");
@@ -79,52 +80,53 @@ public class C206_CaseStudy {
 		System.out.println("5. Quit");
 		Helper.line(80, "-");
 	}
-	
+
 	public static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
 		Helper.line(80, "-");
 	}
-	
-	//Member 1 - Clarence
+
+	// Member 1 - Clarence
 	private static String retrieveMembers(List<Member> members) {
-		String output = String.format("%-10s %-10s %-10s %-10s %-20s %-10s", "Name", "Gender", "Mobile", "Email", "Date of Birth", "Country");
+		String output = String.format("%-10s %-10s %-10s %-10s %-20s %-10s", "Name", "Gender", "Mobile", "Email",
+				"Date of Birth", "Country");
 		for (Member m : members) {
-			output+= m.toString();
+			output += m.toString();
 		}
 		return output;
 	}
-	
+
 	public static void viewMembers(List<Member> members) {
 		setHeader("Members");
 		System.out.println(retrieveMembers(members));
 	}
-	
+
 	public static void addMembers(List<Member> members) {
 		setHeader("Register Member");
 		System.out.println("\n");
-		
+
 		String name = Helper.readString("Enter name > ");
 		char gender = Helper.readChar("Enter gender (M/F) > ");
 		int mobile = Helper.readInt("Enter mobile number > ");
 		String email = Helper.readString("Enter email > ");
 		String dob = Helper.readString("Enter date of birth(DD/MM/YYYY) > ");
 		String country = Helper.readString("Enter Country of residence > ");
-		
+
 		Member newMember = new Member(name, gender, mobile, email, dob, country);
 		members.add(newMember);
 		System.out.println("Member added");
 	}
-	
+
 	public static void deleteMember(List<Member> members) {
 		setHeader("Delete Member");
 		viewMembers(members);
-		
+
 		String name = Helper.readString("Name > ");
 		boolean isFound = false;
 		Member member = null;
 		for (Member m : members) {
-			if(m.getName().equals(name)) {
+			if (m.getName().equals(name)) {
 				isFound = true;
 				member = m;
 				break;
@@ -138,141 +140,127 @@ public class C206_CaseStudy {
 		}
 	}
 
-
-	//Member 2 - Caven
+	// Member 2 - Caven
 	public static void AddCourseCategory(ArrayList<Course> CategoryList) {
 		setHeader("Add Course Category");
-		String category=Helper.readString("Enter a new category name");
-		String description=Helper.readString("Enter description for the new category");
-		String newCat="";
-		Boolean duplicateName=false;
-		
+		String category = Helper.readString("Enter a new category name");
+		String description = Helper.readString("Enter description for the new category");
+		String newCat = "";
+		Boolean duplicateName = false;
+
 		// check for duplicates name
-		for(int i=0 ;i<CategoryList.size();i++) {
-			if(CategoryList.get(i).getCategory().equalsIgnoreCase(category)) {
-				duplicateName=true;
-				System.out.println("Duplicate names Found!");
-				newCat+=Helper.readString("Enter another non-duplicate category name");
-			}
-			else {
-				duplicateName=false;
-			}
-		}
-		//create constructor based on whether there is duplicate
-		if(duplicateName== true) {
-			Course newCategory=new Course(newCat,description);
-			CategoryList.add(newCategory);
-		}
-		else if(duplicateName == false) {
-			Course newCategory=new Course(category,description);
-			CategoryList.add(newCategory);
-		}
-		
-		String output = String.format("%10s %30s\n", "Category name ", "DESCRIPTION"
-				);
 		for (int i = 0; i < CategoryList.size(); i++) {
-			output+=String.format("%10s %30s\n",CategoryList.get(i).getCategory(),CategoryList.get(i).getDescription());
-			
+			if (CategoryList.get(i).getCategory().equalsIgnoreCase(category)) {
+				duplicateName = true;
+				System.out.println("Duplicate names Found!");
+				newCat += Helper.readString("Enter another non-duplicate category name");
+			} else {
+				duplicateName = false;
+			}
+		}
+		// create constructor based on whether there is duplicate
+		if (duplicateName == true) {
+			Course newCategory = new Course(newCat, description);
+			CategoryList.add(newCategory);
+		} else if (duplicateName == false) {
+			Course newCategory = new Course(category, description);
+			CategoryList.add(newCategory);
+		}
+
+		String output = String.format("%10s %30s\n", "Category name ", "DESCRIPTION");
+		for (int i = 0; i < CategoryList.size(); i++) {
+			output += String.format("%10s %30s\n", CategoryList.get(i).getCategory(),
+					CategoryList.get(i).getDescription());
+
 		}
 		System.out.println(output);
-		
+
 	}
-	
 
 	public static void viewAllCourseCategory(ArrayList<Course> CategoryList) {
 		setHeader("Viewing Course Category");
-		String output = String.format("%-10s %-30s\n", "Category name ", "DESCRIPTION"
-				);
-		boolean exist= false;
+		String output = String.format("%-10s %-30s\n", "Category name ", "DESCRIPTION");
+		boolean exist = false;
 		for (int i = 0; i < CategoryList.size(); i++) {
-			output+=String.format("%-10s %-30s\n",CategoryList.get(i).getCategory(),CategoryList.get(i).getDescription());
-			
+			output += String.format("%-10s %-30s\n", CategoryList.get(i).getCategory(),
+					CategoryList.get(i).getDescription());
+
 		}
 		System.out.println(output);
-		
-		String categoryChoose=Helper.readString("Enter the category name to view category");
+
+		String categoryChoose = Helper.readString("Enter the category name to view category");
 		for (int i = 0; i < CategoryList.size(); i++) {
-			if(CategoryList.get(i).getCategory().equalsIgnoreCase(categoryChoose)) {
+			if (CategoryList.get(i).getCategory().equalsIgnoreCase(categoryChoose)) {
 				System.out.println("Category Found !");
-				System.out.println("Category name:"+CategoryList.get(i).getCategory()+"\n"+"Category description:"+CategoryList.get(i).getDescription());
-				exist=true;
+				System.out.println("Category name:" + CategoryList.get(i).getCategory() + "\n" + "Category description:"
+						+ CategoryList.get(i).getDescription());
+				exist = true;
 			}
-			
+
 		}
-		if(exist== false) {
+		if (exist == false) {
 			System.out.println("No such category name!");
 		}
-		
-			
-		}
 
-	
-	public static void DeleteACourseCategory(ArrayList<Course> CategoryList) {
-		setHeader("Deleting Course Category");
-		String result="";
-		boolean exist=false;
-		String output = String.format("%-10s %-30s\n", "Category name ", "DESCRIPTION"
-				);
-		for (int i = 0; i < CategoryList.size(); i++) {
-			output+=String.format("%-10s %-30s\n",CategoryList.get(i).getCategory(),CategoryList.get(i).getDescription());
-			
-		}
-		System.out.println(output);
-		int numberToDelete=0;
-		String category=Helper.readString("Enter a  category name to delete from ");
-		for (int i = 0; i < CategoryList.size(); i++) {
-			if(CategoryList.get(i).getCategory().equalsIgnoreCase(category)) {
-				numberToDelete=i;
-				exist=true;
-			}
-			
-		}
-		if(exist == true) {
-			CategoryList.remove(numberToDelete);
-			result+="Category removed!";
-		}
-		else {
-			result+="No such category found!";
-		}
-		System.out.println(result);
-		
-		
-		
 	}
 
-	
-	
-	
+	public static void DeleteACourseCategory(ArrayList<Course> CategoryList) {
+		setHeader("Deleting Course Category");
+		String result = "";
+		boolean exist = false;
+		String output = String.format("%-10s %-30s\n", "Category name ", "DESCRIPTION");
+		for (int i = 0; i < CategoryList.size(); i++) {
+			output += String.format("%-10s %-30s\n", CategoryList.get(i).getCategory(),
+					CategoryList.get(i).getDescription());
 
-	
+		}
+		System.out.println(output);
+		int numberToDelete = 0;
+		String category = Helper.readString("Enter a  category name to delete from ");
+		for (int i = 0; i < CategoryList.size(); i++) {
+			if (CategoryList.get(i).getCategory().equalsIgnoreCase(category)) {
+				numberToDelete = i;
+				exist = true;
+			}
 
-	//Member 3 - Daryl
+		}
+		if (exist == true) {
+			CategoryList.remove(numberToDelete);
+			result += "Category removed!";
+		} else {
+			result += "No such category found!";
+		}
+		System.out.println(result);
+
+	}
+
+	// Member 3 - Daryl
 	private static String retrieveCourses(List<Course> courses) {
-		String output =  String.format("%-10s %-20s %-20s %-20s %-20s %-20s\n",
-				"Id", "Title", "Category", "Decription", "Duration", "Pre-requisite Course");
+		String output = String.format("%-10s %-20s %-20s %-20s %-20s %-20s\n", "Id", "Title", "Category", "Decription",
+				"Duration", "Pre-requisite Course");
 		for (Course c : courses) {
 			output += c.toString();
 		}
 		return output;
 	}
-	
+
 	public static void viewCourses(List<Course> courses) {
 		setHeader("Courses");
 		System.out.println(retrieveCourses(courses));
 	}
-	
+
 	public static void addCourse(List<Course> courses) {
 		setHeader("Add Course\nEnter Course Information\n");
-		
-		//course information
+
+		// course information
 		String id = Helper.readString("ID : ");
 		String title = Helper.readString("Title : ");
 		String category = Helper.readString("Category Name : ");
 		String desc = Helper.readString("Description : ");
 		double duration = Helper.readDouble("Duration : ");
 		String preCourse = Helper.readString("Pre-requisite Course : ");
-		
-		//check duplicates
+
+		// check duplicates
 		boolean isDuplicate = false;
 		for (Course c : courses) {
 			if (c.getId().equals(id)) {
@@ -280,8 +268,8 @@ public class C206_CaseStudy {
 				break;
 			}
 		}
-		
-		//add course
+
+		// add course
 		if (!isDuplicate) {
 			Course newCourse = new Course(id, title, category, desc, duration, preCourse);
 			courses.add(newCourse);
@@ -290,15 +278,15 @@ public class C206_CaseStudy {
 			System.out.println("Course Already Exists.");
 		}
 	}
-	
+
 	public static void deleteCourse(List<Course> courses) {
 		setHeader("Delete Course");
 		viewCourses(courses);
-		
-		//course id 
+
+		// course id
 		String id = Helper.readString("Course ID : ");
-		
-		//check course id is valid
+
+		// check course id is valid
 		boolean isFound = false;
 		Course course = null;
 		for (Course c : courses) {
@@ -308,18 +296,17 @@ public class C206_CaseStudy {
 				break;
 			}
 		}
-		
-		//delete course
+
+		// delete course
 		if (isFound) {
 			courses.remove(course);
 			System.out.println("Course Deleted.");
 		} else {
 			System.out.println("Invalid Course.");
 		}
-		
+
 	}
-	
-	
+
 	// Member 4 - Sabrina
 	public static String retrieveCourseSchedule(ArrayList<CourseSchedule> scheduleList) {
 		String output = "";
@@ -371,75 +358,49 @@ public class C206_CaseStudy {
 			}
 		}
 	}
-	//Member 5 - Rongxin 
-public static void regSche() {
-		
+
+	// Member 5 - Rongxin
+	public static void regSche() {
+
 		String rn = Helper.readString("Enter a registration number > ");
 		String sid = Helper.readString("Enter a course schedule id > ");
 		String memail = Helper.readString("Enter your email > ");
-		Date rdate = Helper.readDate("Enter a today's date > " );
-		boolean flag = false;
-		String op = "";
-		
-		for(registerSchedule i :list) {
-			if(!rn.equalsIgnoreCase(i.getRegistrationNumber())) {
-			i.setRegistrationNumber(rn);
-			i.setScheduleId(sid);
-			i.setMemberEmail(memail);
-			i.setRegisterDate(rdate);
-			op+= String.format("\n%s\n%s\n%s\n%s\n%s", i.getRegistrationNumber(),i.getScheduleId(),
-					i.getMemberEmail(),i.getStatus(),i.getRegisterDate());
-			flag = true;
-			System.out.println("Register triumph");
-			}else {
+		Date rdate = Helper.readDate("Enter a today's date > ");
+		if (reglist.isEmpty()) {
+			registerSchedule newReg = new registerSchedule(rn, sid, memail, rdate);
+			reglist.add(newReg);
+		}
+		for (registerSchedule i : reglist) {
+			if (!rn.equalsIgnoreCase(i.getRegistrationNumber())) {
+
+				System.out.println("Register triumph");
+			} else {
 				System.out.println("Registration number existed");
 			}
-		}
-		if(flag == true) {
-			saveDetail(op);
-		
+
 		}
 	}
-	
-	static void saveDetail(String result) {
-		System.out.println("Saving to file...");
-		try {
-			File file = new File("RegistrationDetails.txt");
-			FileWriter fw = new FileWriter(file);
-			BufferedWriter bw = new BufferedWriter(fw);
 
-			bw.write(result);
-			bw.close();
-			
-			System.out.println("Detail saved successful!");
-
-		} catch (IOException io) {
-			System.out.println("There was an error saving to the detail.");
-		}
-	}
 	static void viewAllr() {
-		System.out.printf("%-30s %-20s %-20s %-35s %-20s\n","Registration Number",
-				"Schedule ID","Member Email","Status","Registerion Date");
-		for(registerSchedule i :list) {
-			System.out.printf("%-30s %-20s %-20s %-35s %-20s\n",i.getRegistrationNumber(),i.getScheduleId(),
-					i.getMemberEmail(),i.getStatus(),i.getRegisterDate());
+		System.out.printf("%-30s %-20s %-20s %-15s %-20s\n", "Registration Number", "Schedule ID", "Member Email",
+				"Status", "Registerion Date");
+		for (registerSchedule i : reglist) {
+			System.out.printf("%-30s %-20s %-20s %-15s %-20s\n", i.getRegistrationNumber(), i.getScheduleId(),
+					i.getMemberEmail(), i.getStatus(), i.getRegisterDate());
 		}
 	}
+
 	static void delr() {
 		String del = Helper.readString("Enter the registration number to delete > ");
-		for(registerSchedule i :list) {
-			if(del.equalsIgnoreCase(i.registrationNumber)) {
-				i.setRegistrationNumber(null);
-				i.setScheduleId(null);
-				i.setMemberEmail(null);
+		for (registerSchedule i : reglist) {
+			if (del.equalsIgnoreCase(i.registrationNumber)) {
 				i.setStatus("Deleted");
-				i.setRegisterDate(null);
-				
-			}else {
+				System.out.println("Deleted !");
+
+			} else {
 				System.out.println("Invalid registration numbe entered");
 			}
 		}
 	}
-	
 
 }
