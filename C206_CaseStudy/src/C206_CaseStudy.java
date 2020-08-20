f22a781881d6a39e4ad0067f04a75b16d584b036
19020620 <19020620@DESKTOP-UN68PLP>
2020-08-20 11:34:40 +0800

    Member 2-Caven

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
@@ -10,7 +11,45 @@ public class C206_CaseStudy {
 	//Member 1
 	
 	//Member 2
+	ArrayList<Course> CategoryList = new ArrayList<Course>();
+	public static void AddCourseCategory(ArrayList<Course> CategoryList) {
+		String category=Helper.readString("Enter a new category name");
+		String description=Helper.readString("Enter description for the new category");
+		Course newCategory= new Course(category,description);
+		CategoryList.add(newCategory);
+		for (int i = 0; i < CategoryList.size(); i++) {
+			String output = String.format("%-10s %-30s\n", "Category name ", "DESCRIPTION"
+					);
+			output+=String.format("%-10s %-30s\n",CategoryList.get(i).getDescription(),CategoryList.get(i).getDescription());
+			System.out.println(output);
+		}
+		
+	}
 	
+	
+	public static void viewAllCourseCategory(ArrayList<Course> CategoryList) {
+		for (int i = 0; i < CategoryList.size(); i++) {
+			String output = String.format("%-10s %-30s\n", "Category name ", "DESCRIPTION"
+					);
+			output+=String.format("%-10s %-30s\n",CategoryList.get(i).getDescription(),CategoryList.get(i).getDescription());
+			System.out.println(output);
+		}
+		
+		String categoryChoose=Helper.readString("Enter the category name to view category");
+		for (int i = 0; i < CategoryList.size(); i++) {
+			if(CategoryList.get(i).getCategory().equalsIgnoreCase(categoryChoose)) {
+				System.out.println("Category Found !");
+				System.out.println("Category name:"+CategoryList.get(i).getCategory()+"\n"+"Category description:"+CategoryList.get(i).getDescription());
+				
+			}
+			else {
+				System.out.println("No such Category Found !");
+			}
+		}
+		
+			
+		}
+		
 	//Member 3
 	
 	
